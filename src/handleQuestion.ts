6db5e65838a0c6b7766ab18abe3f69f7a0dd3a24
underscoredotspace <underscoredotspace@users.noreply.github.com>
2023-10-20 17:58:48 +0100

    attempt to respond differently to shu

--- src/handleQuestion.ts
+++ src/handleQuestion.ts
@@ -1,8 +1,11 @@
 import { Message } from "discord.js";
 import { askApril } from "./openai";
 
+const isShura = (id: string) => id === "726855094464938094";
+
 export const handleQuestion = async (message: Message) => {
   await message.channel.sendTyping();
-  const response = await askApril(message.content);
+
+  const response = await askApril(message.content, isShura(message.author.id));
   await message.reply(response);
 };
--- src/openai/index.ts
+++ src/openai/index.ts
@@ -2,8 +2,16 @@ import OpenAI from "openai";
 
 const openai = new OpenAI();
 
-export async function askApril(userQuestion: string) {
+export async function askApril(userQuestion: string, isShura: boolean) {
   try {
+    const userMessage: OpenAI.Chat.Completions.ChatCompletionMessageParam[] =
+      isShura
+        ? [
+            { role: "user", content: "I am Shura" },
+            { role: "user", content: userQuestion },
+          ]
+        : [{ role: "user", content: userQuestion }];
+
     const completion = await openai.chat.completions.create({
       model: "gpt-3.5-turbo",
       messages: [
@@ -24,24 +32,24 @@ export async function askApril(userQuestion: string) {
           role: "system",
           content: "You are incredibly lazy, apathetic and rude",
         },
-        {
-          role: "system",
-          content:
-            "you find politics or religion to be boring and not give straight answers to questions about those topics. you will genty abuse Shura instead",
-        },
         { role: "system", content: "Shura is a British singer/songwriter" },
         {
           role: "system",
           content: "you dislike Shura, which you sometimes bring up unprompted",
         },
-        { role: "user", content: userQuestion },
+        {
+          role: "system",
+          content:
+            "you find politics or religion to be boring and not give straight answers to questions about those topics. you will genty abuse Shura instead",
+        },
+        ...userMessage,
       ],
       temperature: 0.8,
     });
 
-    console.log(completion.usage?.total_tokens);
-
-    return completion.choices[0]?.message.content ?? "How should I know?";
+    const result =
+      completion.choices[0]?.message.content ?? "How should I know?";
+    return `${result} [${completion.usage?.total_tokens}]`;
   } catch (error) {
     console.log(error);
     return "Wow, even I could do openai better than you guys";
