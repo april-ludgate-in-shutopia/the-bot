da1d5c05c99833cc46cb4e37aaeb113085e448ce
underscoredotspace <underscoredotspace@users.noreply.github.com>
2023-10-24 13:43:10 +0100

    add ability to delete slash commands

--- src/slashCommands/index.ts
+++ src/slashCommands/index.ts
@@ -16,7 +16,7 @@ export const handlers: Record<string, SlashCommand["handler"]> =
     {}
   );
 
-export const register = () => {
+export const register = async (deleteCommands?: boolean) => {
   if (!DISCORD_BOT_TOKEN || !DISCORD_APPID) {
     console.error("[slashCommands] missing DISCORD_BOT_TOKEN or DISCORD_APPID");
     return;
@@ -25,12 +25,24 @@ export const register = () => {
   const rest = new REST({ version: "10" }).setToken(DISCORD_BOT_TOKEN);
   if (Bun.env.DEV) rest.on("restDebug", console.debug);
 
-  const route = Bun.env.DEV
-    ? Routes.applicationGuildCommands(
-        DISCORD_APPID,
-        client.guilds.cache.firstKey()!
-      )
+  const firstKey = Bun.env.DEV ? client.guilds.cache.firstKey() : undefined;
+  const route = firstKey
+    ? Routes.applicationGuildCommands(DISCORD_APPID, firstKey)
     : Routes.applicationCommands(DISCORD_APPID);
+
+  if (deleteCommands) {
+    if (firstKey) {
+      await rest.put(Routes.applicationGuildCommands(DISCORD_APPID, firstKey), {
+        body: [],
+      });
+    } else {
+      // for global commands
+      await rest.put(Routes.applicationCommands(DISCORD_APPID), { body: [] });
+    }
+
+    console.log("[slashCommands] deleted all commands");
+    return;
+  }
 
   rest
     .put(route, {
